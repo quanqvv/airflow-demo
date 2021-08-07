0606ff402a712727e54e494e1029a589df56134d
quanqvv <69457581+quanqvv@users.noreply.github.com>
2021-08-07 09:04:03 +0700

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Just demo project" + 1/0);
+        System.out.println("Just demo project" + 1/1);
     }
 }
